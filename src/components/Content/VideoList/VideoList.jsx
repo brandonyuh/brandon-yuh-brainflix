500e58f872835bf5b0cda86b7f208e2bf50100aa
Brandon <brandonyuh@gmail.com>
2023-03-01 20:49:12 -0800

    mouse hover video list

--- src/components/Content/VideoList/VideoList.jsx
+++ src/components/Content/VideoList/VideoList.jsx
@@ -1,11 +1,11 @@
 import "./VideoList.scss";
 function VideoList(props) {
   return (
-    <div className="comments">
+    <div className="videolist">
       {props.videos
         .filter((video) => video.id !== props.id)
         .map((video) => (
-          <article key={video.id} onClick={() => props.handleChangeVideo(video.id)}>
+          <article className="videolist__video" key={video.id} onClick={() => props.handleChangeVideo(video.id)}>
             <img src={video.image} alt="" className="videolist__image" />
             <p>{video.title}</p>
             <p>{video.channel}</p>
