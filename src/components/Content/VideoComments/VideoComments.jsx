75cc471b9ad7e5625ffc4ed1f27214f33772fc99
Brandon <brandonyuh@gmail.com>
2023-03-02 10:41:52 -0800

    Added number of comments

--- src/components/Content/VideoComments/VideoComments.jsx
+++ src/components/Content/VideoComments/VideoComments.jsx
@@ -3,6 +3,7 @@ import { convertDate, relativeTime } from "../../../Utilities";
 function VideoComments({ comments }) {
   return (
     <div className="comments">
+      <p>{comments.length} Comments</p>
       {comments.map((comment) => (
         <article key={comment.id} className="comment">
           <p>{comment.name}</p>
