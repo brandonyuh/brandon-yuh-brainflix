c7a3595aa11a5593c4d5ad5f3e500df66bd509ee
Brandon <brandonyuh@gmail.com>
2023-03-02 22:32:48 -0800

    implemented video list CSS for mobile

--- src/components/Content/VideoList/VideoList.jsx
+++ src/components/Content/VideoList/VideoList.jsx
@@ -2,13 +2,19 @@ import "./VideoList.scss";
 function VideoList({ videos, id, handleChangeVideo }) {
   return (
     <div className="videolist">
+      <h3 className="videolist__title">Next Videos</h3>
       {videos
         .filter((video) => video.id !== id)
         .map((video) => (
-          <article className="videolist__video" key={video.id} onClick={() => handleChangeVideo(video.id)}>
-            <img src={video.image} alt="" className="videolist__image" />
-            <p>{video.title}</p>
-            <p>{video.channel}</p>
+          <article className="thumbnail" key={video.id} onClick={() => handleChangeVideo(video.id)}>
+            <div className="thumbnail__preview">
+              <img src={video.image} alt="" className="thumbnail__preview--image" />
+            </div>
+
+            <div className="thumbnail__details">
+              <p className="thumbnail__details--title">{video.title}</p>
+              <p>{video.channel}</p>
+            </div>
           </article>
         ))}
     </div>
