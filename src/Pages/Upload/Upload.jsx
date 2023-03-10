8436523111b9572eb4779ee705c3f03bf34a7585
Brandon <brandonyuh@gmail.com>
2023-03-09 16:54:58 -0800

    changed html tag to a form

--- src/Pages/Upload/Upload.jsx
+++ src/Pages/Upload/Upload.jsx
@@ -14,9 +14,9 @@ function Upload() {
   return (
     <>
       <Header />
-      <section className="upload">
+      <form className="upload">
         <h1 className="upload__title">Upload Video</h1>
-        <article className="upload__container">
+        <section className="upload__container">
           <div className="upload__preview--container">
             <label className="label">Video Thumbnail</label>
             <img className="upload__preview--image" src={UploadImage} alt="" />
@@ -27,16 +27,16 @@ function Upload() {
             <label className="label upload__label">Add a video description</label>
             <textarea className="textbox upload__input upload__input--description" rows="4" placeholder="Add a description to your video" />
           </div>
-        </article>
-        <article className="upload__buttons">
+        </section>
+        <section className="upload__buttons">
           <button className="button upload__buttons--publish" onClick={alertThankUpload}>
             Publish
           </button>
           <Link to="/" className="upload__buttons--cancel">
             Cancel
           </Link>
-        </article>
-      </section>
+        </section>
+      </form>
     </>
   );
 }
