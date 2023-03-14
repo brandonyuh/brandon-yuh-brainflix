3255a1c5bbb73dd1c367c50047103c20d4f8e1a7
Brandon <brandonyuh@gmail.com>
2023-03-14 14:14:33 -0700

    Set a fallback video

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,8 +4,7 @@ function VideoPlayer({ image, video }) {
   return (
     <>
       <div className="video">
-        <video className="video__player" controls poster={image} src={`${video}${apiParams}`}>
-        </video>
+        <video className="video__player" controls poster={image} src={video ? `${video}${apiParams}` : "https://project-2-api.herokuapp.com/stream?api_key=1"}></video>
       </div>
     </>
   );
