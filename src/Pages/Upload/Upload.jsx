dad71a743d9e47f2149121434077262f03e25e9e
Brandon <brandonyuh@gmail.com>
2023-03-13 21:36:46 -0700

    Implemented adding videos

--- src/Pages/Upload/Upload.jsx
+++ src/Pages/Upload/Upload.jsx
@@ -1,16 +1,44 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
 import "./Upload.scss";
 import Header from "../../components/Header/Header";
 import UploadImage from "../../assets/images/Upload-video-preview.jpg";
+import { apiUrl, apiParams } from "../../Api";
 
 function Upload() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("Thank you for uploading your video!");
-    //navigates to home page
-    navigate("/");
+    if(!title || !description) {
+      alert("Please fill out all fields");
+      return;
+    }
+    const newVideo = {
+      title: title,
+      description: description,
+    };
+    //sends new video to server
+    axios.post(`${apiUrl}videos${apiParams}`, newVideo).then((response) => {
+      alert("Thank you for uploading your video!");
+      //navigates to home page
+      navigate("/");
+    }).catch((error) => {
+      console.log(error);
+      alert("Sorry, your video could not be uploaded. Please try again later.");
+    });
   };
 
   return (
@@ -25,9 +53,9 @@ function Upload() {
           </div>
           <div className="upload__inputs">
             <label className="label upload__label">Title your video</label>
-            <input className="textbox upload__input" type="text" placeholder="Add a title to your video" />
+            <input className="textbox upload__input" type="text" placeholder="Add a title to your video" onChange={handleChangeTitle} value={title}/>
             <label className="label upload__label">Add a video description</label>
-            <textarea className="textbox upload__input upload__input--description" rows="4" placeholder="Add a description to your video" />
+            <textarea className="textbox upload__input upload__input--description" rows="4" placeholder="Add a description to your video" onChange={handleChangeDescription} value={description}/>
           </div>
         </section>
         <section className="upload__buttons">
