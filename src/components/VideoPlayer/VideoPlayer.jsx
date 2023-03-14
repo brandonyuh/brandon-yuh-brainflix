f2da9742f956a684a8d3fde9a1a07ab4ce0faa39
Brandon <brandonyuh@gmail.com>
2023-03-14 15:12:48 -0700

    Made the video loop

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,7 +4,7 @@ function VideoPlayer({ image, video }) {
   return (
     <>
       <div className="video">
-        <video className="video__player" controls poster={image} src={video ? `${video}${apiParams}` : "https://project-2-api.herokuapp.com/stream?api_key=1"}></video>
+        <video loop className="video__player" controls poster={image} src={video ? `${video}${apiParams}` : "https://project-2-api.herokuapp.com/stream?api_key=1"}></video>
       </div>
     </>
   );
