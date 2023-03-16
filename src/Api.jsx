853e777475d5ef122e6c44aae543565b289750e9
Brandon <brandonyuh@gmail.com>
2023-03-16 01:24:49 -0700

    Set back to own api

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,9 @@
-//const apiUrl = "http://localhost:5050/";
-const apiUrl = "https://project-2-api.herokuapp.com/";
-//const apiParams = "";
-const apiParams = "?api_key=3bd9fd1c-d227-4f83-9069-b439b85d08c1";
+const apiUrl = "http://localhost:5050/";
+//const apiUrl = "https://project-2-api.herokuapp.com/";
+const apiParams = "";
+//const apiParams = "?api_key=3bd9fd1c-d227-4f83-9069-b439b85d08c1";
 
-export { apiUrl, apiParams };
+const backupUrl = "https://project-2-api.herokuapp.com/";
+const backupParams = "?api_key=3bd9fd1c-d227-4f83-9069-b439b85d08c1";
+
+export { apiUrl, apiParams, backupUrl, backupParams };
