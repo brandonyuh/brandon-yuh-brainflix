b4eb025e35f719594076da7878e640a76532d284
Brandon <brandonyuh@gmail.com>
2023-03-16 22:10:01 -0700

    Clean up api file

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,5 @@
 const apiUrl = "http://localhost:5050/";
-//const apiUrl = "https://project-2-api.herokuapp.com/";
 const apiParams = "";
-//const apiParams = "?api_key=3bd9fd1c-d227-4f83-9069-b439b85d08c1";
 
 const backupUrl = "https://project-2-api.herokuapp.com/";
 const backupParams = "?api_key=3bd9fd1c-d227-4f83-9069-b439b85d08c1";
